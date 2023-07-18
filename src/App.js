f1c85eb9376829f6fb0f1f9ac81b1725af6fd450
Kyoko Akatsuka <kyokoakatsuka38@gmail.com>
2023-07-18 11:40:13 +0900

    []忘れ

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import { useState } from "react";
 import React from "react";
 
 function Square({ value, onSquareClick }) {
-  return (
-    <button className="square" onClick={onSquareClick}>
-      {value}
-    </button>
-  );
+	return (
+		<button className='square' onClick={onSquareClick}>
+			{value}
+		</button>
+	);
 }
 
 function Board({ xIsNext, squares, onPlay }) {
@@ -55,7 +55,7 @@ function Board({ xIsNext, squares, onPlay }) {
 
 export default function Game() {
 	const [xIsNext, setXIsNext] = useState(true);
-	const [history, setHistory] = useState(Array(9).fill(null));
+	const [history, setHistory] = useState([Array(9).fill(null)]);
 	const currentSquares = history[history.length - 1];
 
 	function handlePlay(nextSquares) {
@@ -76,21 +76,21 @@ export default function Game() {
 }
 
 function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+	for (let i = 0; i < lines.length; i++) {
+		const [a, b, c] = lines[i];
+		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+			return squares[a];
+		}
+	}
+	return null;
 }
